a1083c020aa03e9a63fedc4caf14f23733e0fddd
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-08-12 01:13:55 +0800

    v1.0

--- background.js
+++ background.js
@@ -22,7 +22,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
             // 从存储中获取域名列表
             chrome.storage.sync.get(['domains'], function (result) {
                 var domains = result.domains || [];
-                console.log(domains);
                 var url = new URL(tab.url);
                 if (domains.includes(url.hostname)) {
                   chrome.scripting.executeScript({
--- popup.js
+++ popup.js
@@ -1,36 +1,52 @@
-document.getElementById('hideImagesButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        function: hideImages
-      });
-    });
-  });
-  
-  document.getElementById('showImagesButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        function: showImages
-      });
-    });
-  });
-  
-  function hideImages() {
-    var images = document.getElementsByTagName('img');
-    for (var i = 0; i < images.length; i++) {
-      images[i].style.display = 'none';
-    }
+document.getElementById('hideImagesButton').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentHost = (new URL(tabs[0].url)).hostname;
+    console.log(currentHost);
+    chrome.storage.sync.get(['domains'], function (result) {
+      if (!result.domains || !result.domains.includes(currentHost)) {
+        var domains = result.domains ? [...result.domains] : [];
+        domains.push(currentHost);
+        chrome.storage.sync.set({ domains: domains }, function () {
+        });
 
-      chrome.storage.sync.get(['domains'], function(result) {
-                var domains = result.domains || [];
-                console.log(domains);
-              });
+        console.log(currentHost);
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: hideImages
+        });
+      }
+    });
+  });
+});
+
+document.getElementById('showImagesButton').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let currentHost = (new URL(tabs[0].url)).hostname;
+    chrome.storage.sync.get(['domains'], function (result) {
+      if (result.domains.includes(currentHost)) {
+        let domains = [...result.domains]
+        domains = domains.filter(e => e !== currentHost);
+        chrome.storage.sync.set({ domains: domains });
+
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: showImages
+        });
+      }
+    });
+  });
+});
+
+function hideImages() {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
   }
-  
-  function showImages() {
-    var images = document.getElementsByTagName('img');
-    for (var i = 0; i < images.length; i++) {
-      images[i].style.display = '';
-    }
+}
+
+function showImages() {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.display = '';
   }
+}
